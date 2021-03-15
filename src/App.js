00e015ac87ccd6d539fc8f73ca72cf87c9063526
Antti Turunen <asturune@hotmail.com>
2021-03-15 11:12:00 +0200

    Notification komponentti muutettu käyttämään connectia hookieiden sijaan

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import React, { useEffect } from 'react'
 //import { useSelector, useDispatch } from 'react-redux'
 import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
-import { Notification } from './components/Notification'
+//import { Notification } from './components/Notification'
+
+import ConnectedNotification from './components/Notification'
 import { Filter } from './components/AnecdoteFilter'
 
 //------------NÄMÄ TARVITAAN INITIAL TILAN LUOMISEKSI/db.json FILESTÄ DATAN HAKEMISEKSI ALKAA--------
@@ -79,17 +81,32 @@ const App = () => {
     }
   */
   const dispatch = useDispatch()
-  
+
   useEffect(() => {
     dispatch(initializeAnecdotes())
     //Jos ei lisätä hakasulkeiden sisään "dispatch" tulee eslint herja
     //toimisi ilman "dispatch" tekstiä myös vaikka herja jäisikin
   }, [dispatch])
 
-
+  /*
+    return (
+      <div>
+        <Notification />
+        <h2>Filter anecdote</h2>
+        <Filter />
+        <br></br>
+        <h2>Anecdotes</h2>
+        <AnecdoteList />
+        <h2>create new</h2>
+        <AnecdoteForm />
+      </div>
+    )
+  }
+  */
+  //----KUN KÄYTETÄÄN CONNECTEDIA useSelector JA useDispatch HOOKIEN SIJAAN ALKAA--------
   return (
     <div>
-      <Notification />
+      <ConnectedNotification />
       <h2>Filter anecdote</h2>
       <Filter />
       <br></br>
@@ -100,5 +117,6 @@ const App = () => {
     </div>
   )
 }
+//----KUN KÄYTETÄÄN CONNECTEDIA useSelector JA useDispatch HOOKIEN SIJAAN LOPPUU--------
 
 export default App
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,10 @@
 
 import React from 'react'
 import { useSelector } from 'react-redux'
+//Connect importataan käyttöön
+import { connect } from 'react-redux'
 
+/*
 //Notificaatio joka renderöidään. Tähän viittaus "App.js" filessä
 //"App.js" filessä ensin importattu käyttöön
 const Notification = () => {
@@ -26,7 +29,7 @@ const Notification = () => {
         {notification.value}
       </div>
     )
-  } 
+  }
   //Jos ei ole lisätty uutta anecdotea tai votetettu tai 5sec kulunut
   //notificaation näyttämisestä, niin ei renderöidä mitään eli arvo on null
   //tai muutettu ohjelmallisesti null:ksi
@@ -34,4 +37,55 @@ const Notification = () => {
     return null
   }
 }
-export { Notification }
+*/
+
+//------------TÄSSÄ KÄYTETÄÄN CONNECTIA ALKAA-------------------------------
+const Notification = (props) => {
+
+  //const notification = useSelector(state => state.notification)
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  //Ks. notificationReducder.js filestä 
+  //const initialState = { value: 'INITIAL NOTIFICATION' }
+  //siksi alla tarvitaan "notification.value"
+  console.log('MIKÄ ARVO NOTIFICAATIOLLA ', props.notification.value)
+  //Jos on luotu uusi anecdote, notificaation tilan arvo ei ole null
+  //ja viestin sisältö määräytyy "AnecdoteForm.js" ja "notificationReducder.js"
+  //fileistä
+  if (props.notification.value !== null) {
+    return (
+      <div style={style}>
+        {props.notification.value}
+      </div>
+    )
+  }
+  //Jos ei ole lisätty uutta anecdotea tai votetettu tai 5sec kulunut
+  //notificaation näyttämisestä, niin ei renderöidä mitään eli arvo on null
+  //tai muutettu ohjelmallisesti null:ksi
+  else if (props.notification.value === null) {
+    return null
+  }
+}
+
+//Tällä funktiolla välitetään komponentille tila/state propseina
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+
+  }
+}
+
+//Moduuli eksporttaa nyt alkuperäisen komponentin sijaan 
+//yhdistetyn komponentin.
+//Tällä saadaan välitettyä Notification komponentille "mapStateToProps" -funktio
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
+
+//------------TÄSSÄ KÄYTETÄÄN CONNECTIA LOPPUU-------------------------------
+
+//----------Kun käytetään connect:a, kommentoidaan tämä pois------
+//export { Notification }
